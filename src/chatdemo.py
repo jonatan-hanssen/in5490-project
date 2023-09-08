337e0a89197c842ac6a846852f3bc0a7fa62fca3
Jonatan Hoffmann Hanssen <jonatahh@dancer.ifi.uio.no>
2023-09-08 16:21:48 +0200

    Simplify chatdemo program
    Signed-off-by: Jonatan Hoffmann Hanssen <jonatahh@dancer.ifi.uio.no>

--- src/chatdemo.py
+++ src/chatdemo.py
@@ -1,74 +1,35 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
 
-from typing import List, Optional
-import time
+from llama import Llama
+import os
 
-import fire
+base_path = os.path.dirname(__file__)
 
-from llama import Llama, Dialog
+generator = Llama.build(
+    ckpt_dir=os.path.join(base_path, "../llama-2-7b-chat"),
+    tokenizer_path=os.path.join(base_path, "../llama-2-7b-chat/tokenizer.model"),
+    max_seq_len=512,
+    max_batch_size=6,
+)
 
+while True:
+    inp = input("USER: ")
 
-def main(
-    ckpt_dir: str,
-    tokenizer_path: str,
-    temperature: float = 0.6,
-    top_p: float = 0.9,
-    max_seq_len: int = 512,
-    max_batch_size: int = 8,
-    max_gen_len: Optional[int] = 10,
-):
-    """
-    Entry point of the program for generating text using a pretrained model.
+    dialogs = [
+        [
+            {
+                "role": "user",
+                "content": inp,
+            }
+        ],
+    ]
 
-    Args:
-        ckpt_dir (str): The directory containing checkpoint files for the pretrained model.
-        tokenizer_path (str): The path to the tokenizer model used for text encoding/decoding.
-        temperature (float, optional): The temperature value for controlling randomness in generation.
-            Defaults to 0.6.
-        top_p (float, optional): The top-p sampling parameter for controlling diversity in generation.
-            Defaults to 0.9.
-        max_seq_len (int, optional): The maximum sequence length for input prompts. Defaults to 512.
-        max_batch_size (int, optional): The maximum batch size for generating sequences. Defaults to 8.
-        max_gen_len (int, optional): The maximum length of generated sequences. If None, it will be
-            set to the model's max sequence length. Defaults to None.
-    """
-    generator = Llama.build(
-        ckpt_dir=ckpt_dir,
-        tokenizer_path=tokenizer_path,
-        max_seq_len=max_seq_len,
-        max_batch_size=max_batch_size,
+    results = generator.chat_completion(
+        dialogs,  # type: ignore
+        max_gen_len=15,
+        temperature=0.6,
+        top_p=0.9,
     )
-
-    while True:
-        inp = input("USER: ")
-
-        dialogs: List[Dialog] = [
-            [
-                {
-                    "role": "user",
-                    "content": inp,
-                }
-            ],
-        ]
-
-        start = time.time()
-        results = generator.chat_completion(
-            dialogs,  # type: ignore
-            max_gen_len=max_gen_len,
-            temperature=temperature,
-            top_p=top_p,
-        )
-        print(f"Time taken for inference: {time.time() - start}")
-
-        for dialog, result in zip(dialogs, results):
-            for msg in dialog:
-                print(f"{msg['role'].capitalize()}: {msg['content']}\n")
-            print(
-                f"> {result['generation']['role'].capitalize()}: {result['generation']['content']}"
-            )
-            print("\n==================================\n")
-
-
-if __name__ == "__main__":
-    fire.Fire(main)
+    output = results[0]["generation"]["content"]
+    print(output)
--- startchat.sh
+++ startchat.sh
@@ -1,1 +1,1 @@
-torchrun --nproc_per_node 1 src/chatdemo.py --ckpt_dir llama-2-7b-chat/ --tokenizer_path llama-2-7b-chat/tokenizer.model --max_seq_len 512 --max_batch_size 6
+torchrun --nproc_per_node 1 src/chatdemo.py
