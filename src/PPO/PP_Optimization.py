180e4d95d8d28b815cca0cc4007962883b63fb8d
Gregz9 <gregorkajda@gmail.com>
2023-10-16 12:05:20 +0200

    Update to structure of the PP_Optimization.py

--- src/PPO/PP_Optimization.py
+++ src/PPO/PP_Optimization.py
@@ -10,6 +10,23 @@ import gymnasium as gym
 import torch.nn as nn
 import torch.optim as optim
 from torch.distributions.categorical import Categorical
+import json
+
+
+def read_params():
+    file = open("hyperparams.json")
+    params = json.load(file)
+    # print(json.dumps(params, indent=4, separators=(":", ",")))
+    return params
+
+
+def save_params(params):
+    file = open("hyperparams.json", "w")
+    json.dump(params, file, indent=4, separators=(",", ":"))
+
+
+def sample_minibatches():
+    pass
 
 
 def init_weightsNbias(layer, std=np.sqrt(2), bias_const=0.0):
@@ -43,6 +60,7 @@ def make_env(env_name, seed):
 
     return env_gen
 
+
 # TODO: Transfer model to device a.k.a. to GPU
 class Agent(nn.Module):
     """
@@ -52,9 +70,9 @@ class Agent(nn.Module):
         the agent is in.
     """
 
-    def __init__(self, envs):
-        super(Agent, self).__init__()
-        self.critic = nn.Sequential(
+    def __init__(, envs):
+        super(Agent, ).__init__()
+        .critic = nn.Sequential(
             init_weightsNbias(
                 nn.Linear(
                     np.array(
@@ -69,7 +87,7 @@ class Agent(nn.Module):
             init_weightsNbias(nn.Linear(64, 1), std=1.0),
         )
 
-        self.actor = nn.Sequential(
+        .actor = nn.Sequential(
             init_weightsNbias(
                 nn.Linear(
                     np.array(
@@ -85,215 +103,220 @@ class Agent(nn.Module):
         )
 
         # Yet to be integrated -> shall serve as the second actor
-        # self.LM_actor = llama2_7b_policy()
+        # .LM_actor = llama2_7b_policy()
 
-    def get_value(self, X):
-        return self.critic(X)
+    def get_value(, X):
+        return .critic(X)
 
-    def get_action_and_value(self, X, action=None):
-        logits = self.actor(X)
+    def get_action_and_value(, X, action=None):
+        logits = .actor(X)
         probs = Categorical(logits=logits)
         if action is None:
             action = probs.sample()
-        return action, probs.log_prob(action), probs.entropy(), self.critic(X)
+        return action, probs.log_prob(action), probs.entropy(), .critic(X)
 
 
-class PPO:
-    def __init__(self, args):
-        # TODO: Replace Argparse with a json containing all parameters and clean up __init__
-        self.args = args()
-        self.seeding()
-        # TODO: Remove gymnasium vectorization of environment
-        seeds = [random.randint(0, 20000) for _ in range(env_n)]
-        self.envs = gym.vector.SyncVector(
-            [make_env(env_name, seeds[i]) for i in range(self.args.num_envs)]
-        )
-        self.agent = Agent(envs).to(device)
-        self.optimizer = optim.Adam(agent.parameters(), lr=self.args.lr, eps=1e-5)
-        _, _ = envs.reset(seed=self.args.seed)
-        self.rollouts = self.args.tot_steps // self.args.batch_size
+def PPO(param_path):
+    args = read_params()
+    seeding()
+    seeds = [random.randint(0, 20000) for _ in range(args["env_n"])]
+    envs = gym.vector.SyncVector(
+        [make_env(env_name, seeds[i]) for i in range(args["num_envs"])]
+    )
+    agent = Agent(envs).to(device)
+    optimizer = optim.Adam(agent.parameters(), lr=args.lr, eps=1e-5)
+    _, _ = envs.reset(seed=args["seed"])
+    args["rollouts"] = args["tot_steps"] // args["batch_size"]
 
-    def save_model(self, model):
-        file_path = os.path.dirname(__file__) + "/model.zip"
-        torch.save(self.agent.state_dict(), file_path)
 
-    def load_model(self, model):
-        file_path = os.path.dirname(__file__) + "/model.zip"
-        model.load_state_dict(torch.load(file_path))
+def save_model(model):
+    file_path = os.path.dirname(__file__) + "/model.zip"
+    torch.save(model.state_dict(), file_path)
 
-    def train(self):
-        # Containers for values needed in calculation of surrogate loss
-        self.single_obs_shape = (
-            int(np.array(envs.single_observation_space["image"].shape).prod()),
-        )
 
-        self.observations = torch.zeros(
-            (self.args.ep_steps, self.args.num_envs, single_obs_shape),
-        ).to(device)
-        self.actions = torch.zeros(
-            (self.args.ep_steps, self.args.num_envs) + self.envs.single_action_space
-        ).to(device)
-        # The ActorCritic Network outputs log probabilities
-        self.logprobs = torch.zeros((self.args.ep_steps, self.args.num_envs)).to(device)
-        self.rewards = torch.zeros((self.args.ep_steps, self.args.num_envs)).to(device)
-        self.dones = torch.zeros((self.args.ep_steps, self.args.num_envs)).to(device)
-        self.values = torch.zeros((self.args.ep_steps, self.args.num_envs)).to(device)
+def load_model(model):
+    file_path = os.path.dirname(__file__) + "/model.zip"
+    model.load_state_dict(torch.load(file_path))
 
-        # Initializing the next step
-        # next_observation = torch.zeros(self.envs.reset()[0]).to(device)
-        # next_done = torch.zeros(self.args.num_envs).to(device)
 
-        # Episode: Moving n steps and estimating the value funvtion for each step
-        for rollout in range(self.rollouts + 1):
-            self.forward(rollout)
-            self.backward()
+def PPO_train(envs):
+    # Containers for values needed in calculation of surrogate loss
+    single_obs_shape = (
+        int(np.array(envs.single_observation_space["image"].shape).prod()),
+    )
 
-    def forward(self, rollout_num):
-        for step in range(self.args.ep_steps):
-            self.global_step += self.args.num_envs
-            if rollout_num == 0:
-                self.observations[step] = torch.zeros(self.envs.reset()[0]).to(device)
-                self.dones[step] = torch.zeros(self.args.num_envs).to(device)
+    observations = torch.zeros(
+        (args["ep_steps"], args["num_envs"], single_obs_shape),
+    ).to(device)
+    actions = torch.zeros(
+        (args["ep_steps"], args["num_envs"]) + envs.single_action_space
+    ).to(device)
+    # The ActorCritic Network outputs log probabilities
+    logprobs = torch.zeros((args["ep_steps"], args["num_envs"])).to(device)
+    rewards = torch.zeros((args["ep_steps"], args["num_envs"])).to(device)
+    dones = torch.zeros((args["ep_steps"], args["num_envs"])).to(device)
+    values = torch.zeros((args["ep_steps"], args["num_envs"])).to(device)
 
-            with torch.no_grad():
-                action, logprob, _, value = self.agent.get_action_and_value(
-                    next_observation
-                )
-                self.values[step] = value.flatten()
-            self.actions[step] = action
-            self.logprobs[step] = logprob
+    # Initializing the next step
+    # next_observation = torch.zeros(.envs.reset()[0]).to(device)
+    # next_done = torch.zeros(.args.num_envs).to(device)
 
-            next_observation, reward, done, truncated, info = envs.step(
-                action.cpu().numpy()
-            )
-            # Converting to tensor and transfering to gpu for faster computation
-            self.rewards[step] = torch.Tensor(reward).to(device).view(-1)
+    # Episode: Moving n steps and estimating the value funvtion for each step
+    for rollout in range(args["rollouts"] + 1):
+        env_episode(rollout)
+        comp_advantage(gae=args["gae"])
+        PPO_update()
 
-            next_observation, next_done = (
-                torch.Tensor(next_observation).to(device),
-                torch.Tensor(next_done).to(device),
-            )
 
-        # Now that the agent(-s) has(-ve) played out an episode, it's time
-        # to backtrack all steps, and compute the discounted rewards
+def env_episode(rollout_num, agent):
+    for step in range(args["ep_steps"]):
+        global_step += args["num_envs"]
+        if rollout_num == 0:
+            observations[step] = torch.zeros(envs.reset()[0]).to(device)
+            dones[step] = torch.zeros(args["num_envs"]).to(device)
+
         with torch.no_grad():
-            next_value = agent.get_value(next_observation).reshape(1, -1)
-            # General advanatage estimation proposed in the original paper on PPO
-            if self.args.gae:
-                returns = torch.zeros_like(rewards).to(device)
-                # lastgaelam = 0
-                for t in reversed(range(self.args.num_steps)):
-                    if t == self.args.num_steps - 1:
-                        # "nextnonterminal" is a environment specific variable indicating if the
-                        # agent has finished the game before reaching time step limit
-                        nextnonterminal = 1.0 - next_done
-                        nextvalues = next_value
-                    else:
-                        nextnonterminal = 1.0 - self.dones[t + 1]
+            action, logprob, _, value = agent.get_action_and_value(next_observation)
+            values[step] = value.flatten()
+        actions[step] = action
+        logprobs[step] = logprob
 
-                    delta = (
-                        rewards[t]
-                        + self.args.gamma * values[t + 1] * nextnonterminal
-                        - values[t]
-                    )
-                    self.returns[t] = (
-                        delta
-                        + self.args.gamma
-                        * self.args.gae_lambda
-                        * nextnonterminal
-                        * self.returns[t + 1]
-                    )
-                self.advantages = self.returns + self.values
-                # Standard advantage estimation of General Advantage Estimation
-                # proposed by the authors of PPO is not used
-            else:
-                returns = torch.zeros_like(rewards).to(device)
-                for t in reversed(range(self.args.num_steps)):
-                    if t == self.args.num_steps - 1:
-                        nextnonterminal = 1.0 - next_done
-                        nextvalues = next_value
-                    else:
-                        nextnonterminal = 1.0 - self.dones[t + 1]
+        next_observation, reward, done, truncated, info = envs.step(
+            action.cpu().numpy()
+        )
+        # Converting to tensor and transfering to gpu for faster computation
+        rewards[step] = torch.Tensor(reward).to(device).view(-1)
 
-                    returns = (
-                        self.rewards[t]
-                        * self.args.gamma
-                        * nextnonterminal
-                        * returns[t + 1]
-                    )
-                self.advantages = returns - self.values
+        next_observation, next_done = (
+            torch.Tensor(next_observation).to(device),
+            torch.Tensor(next_done).to(device),
+        )
 
-    def backward(self):
-        # Optimization of the surrogate loss
+def comp_advantage(gae=False):
+    # Now that the agent(-s) has(-ve) played out an episode, it's time
+    # to backtrack all steps, and compute the discounted rewards
+    with torch.no_grad():
+        next_value = agent.get_value(next_observation).reshape(1, -1)
+        # General advanatage estimation proposed in the original paper on PPO
+        if gae:
+            returns = torch.zeros_like(rewards).to(device)
+            # lastgaelam = 0
+            for t in reversed(range(args["num_steps"])):
+                if t == args["num_steps"] - 1:
+                    # "nextnonterminal" is a environment specific variable indicating if the
+                    # agent has finished the game before reaching time step limit
+                    nextnonterminal = 1.0 - next_done
+                    nextvalues = next_value
+                else:
+                    nextnonterminal = 1.0 - dones[t + 1]
 
-        # Flattening all containers in order to compute components of surrogate losses
-        # using minibatches
-        batch_observations = self.observations.view(
-            (-1,) + self.single_obs_shape
-        )  # if "view(-1) does not work, use reshape()
-        batch_logprobs = self.logprobs.view(-1)
-        batch_actions = self.actions.view((-1,) + envs.single_action_space.shape)
-        batch_advantages = self.advantages.view(-1)
-        batch_values = self.values.view(-1)
-        batch_returns = self.returns.view(-1)
+                delta = (
+                    rewards[t]
+                    + args["gamma"] * values[t + 1] * nextnonterminal
+                    - values[t]
+                )
+                returns[t] = (
+                    delta
+                    + args["gamma"]
+                    * args["gae_lambda"]
+                    * nextnonterminal
+                    * returns[t + 1]
+                )
+            advantages = ret  values
+            # Standard advantage estimation of General Advantage Estimation
+            # proposed by the authors of PPO is not used
+        else:
+            returns = torch.zeros_like(rewards).to(device)
+            for t in reversed(range(args.num_steps)):
+                if t == args["num_steps"] - 1:
+                    nextnonterminal = 1.0 - next_done
+                    nextvalues = next_value
+                else:
+                    nextnonterminal = 1.0 - dones[t + 1]
 
-        # batch_idx = np.arange(self.args.input_size)
-        for epoch in range(self.args.epochs):
-            # Shuffle the batch indexes to introduce additional noise and variance
-            # np.random.shuffle(batch_idx)
-            batch_idx = np.random.choice(
-                self.args.batch_size, self.args.batch_size, replace=False
+                returns[t] = (
+                    rewards[t]
+                    * args["gamma"]
+                    * nextnonterminal
+                    * returns[t + 1]
+                )
+            advantages = returns - values
+
+
+def backward():
+    # Optimization of the surrogate loss
+
+    # Flattening all containers in order to compute components of surrogate losses
+    # using minibatches
+    batch_observations = .observations.view(
+        (-1,) + .single_obs_shape
+    )  # if "view(-1) does not work, use reshape()
+    batch_logprobs = .logprobs.view(-1)
+    batch_actions = .actions.view((-1,) + envs.single_action_space.shape)
+    batch_advantages = .advantages.view(-1)
+    batch_values = .values.view(-1)
+    batch_returns = .returns.view(-1)
+
+    # batch_idx = np.arange(.args.input_size)
+    for epoch in range(.args.epochs):
+        # Shuffle the batch indexes to introduce additional noise and variance
+        # np.random.shuffle(batch_idx)
+        # TODO: Implement a minibatch generator here -> Gregz
+        batch_idx = np.random.choice(
+            args["batch_size"], args["batch_size"], replace=False
+        )
+        for start in range(0, args["batch_size"], args.["minibatch_size"]):
+            end = start + args["batch_size"]
+            minibatch_idx = batch_idx[start:end]
+
+            newAction, newLogprob, entropy, newValue = agent.get_action_and_value(
+                batch_observations[minibatch_idx],
+                batch_actions.long()[minibatch_idx],
             )
-            for start in range(0, self.args.batch_size, self.args.minibatch_size):
-                end = start + self.args.batch_size
-                minibatch_idx = batch_idx[start:end]
 
-                newAction, newLogprob, entropy, newValue = agent.get_action_and_value(
-                    batch_observations[minibatch_idx],
-                    batch_actions.long()[minibatch_idx],
-                )
+            # The probability ratio of the new policy vs the old policy
+            policy_ratio = (newLogprob - batch_logprobs[minibatch_idx]).exp()
 
-                # The probability ratio of the new policy vs the old policy
-                policy_ratio = (newLogprob - batch_logprobs[minibatch_idx]).exp()
+            minibatch_advantages = batch_advantages[minibatch_idx]
+            # TODO: Add normalization of minibatch_advantage
 
-                minibatch_advantages = batch_advantages[minibatch_idx]
-                # TODO: Add normalization of minibatch_advantage
+            pGrad_clip1 = policy_ratio * minibatch_advantages
+            pGrad_clip2 = minibatch_advantages * torch.clamp(
+                policy_ratio,
+                1.0 - args["clip_epislon"],
+                1.0 + args["clip_epislon"],
+            )
+            # Whenever a paper writes an equation as the expected value, take the mean() of that function
+            # to imitate the expected value
+            clip_Loss = torch.min(pGrad_clip1, pGrad_clip2).mean()
 
-                pGrad_clip1 = policy_ratio * minibatch_advantages
-                pGrad_clip2 = minibatch_advantages * torch.clamp(
-                    policy_ratio,
-                    1.0 - self.args.clip_epislon,
-                    1.0 + self.args.clip_epislon,
-                )
-                # Whenever a paper writes an equation as the expected value, take the mean() of that function
-                # to imitate the expected value
-                clip_Loss = torch.min(pGrad_clip1, pGrad_clip2).mean()
+            # Computing Value Loss
+            value_loss = (
+                (1 / 2)
+                * ((newValue.view(-1) - batch_values[minibatch_idx]) ** 2).mean()
+                * args["coef1"]
+            )
 
-                # Computing Value Loss
-                value_loss = (
-                    (1 / 2)
-                    * ((newValue.view(-1) - batch_values[minibatch_idx]) ** 2).mean()
-                    * self.args.coef1
-                )
+            # Computing Entropy Loss
+            entropy_loss = entropy.mean() * args["coef2"]
 
-                # Computing Entropy Loss
-                entropy_loss = entropy.mean() * self.args.coef2
+            surrogate_loss = clip_Loss - value_loss + entropy_loss
+            optimizer.zero_grad()
+            loss.backward()
+            nn.utils.clip_grad_norm(
+                agent.parameters(), args["max_grad_norm"]
+            )
+            .optimizer.step()
 
-                surrogate_loss = clip_Loss - value_loss + entropy_loss
-                self.optimizer.zero_grad()
-                loss.backward()
-                nn.utils.clip_grad_norm(
-                    self.agent.parameters(), self.args.max_grad_norm
-                )
-                self.optimizer.step()
-
-    def seeding(self):
-        random.seed(self.args.seed)
-        np.random.seed(self.args.seed)
-        torch.manual_seed(self.args.seed)
-        torch.backends.cudnn.deterministic = True
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+def seeding():
+    random.seed(.args.seed)
+    np.random.seed(.args.seed)
+    torch.manual_seed(.args.seed)
+    torch.backends.cudnn.deterministic = True
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 if __name__ == "__main__":
     print("The Main function")
+
+    read_params()
