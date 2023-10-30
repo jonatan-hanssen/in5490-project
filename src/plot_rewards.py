643166c7920687e8d7c8fe5913d0ca0a5769ed63
Jonatan Hanssen <jonatan.hanssen@outlook.com>
2023-10-30 09:29:51 +0100

    Add more paper
    Signed-off-by: Jonatan Hanssen <jonatan.hanssen@outlook.com>

--- src/plot_rewards.py
+++ src/plot_rewards.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os, argparse, glob
 
-plt.rcParams.update({"font.size": 22})
+plt.rcParams.update({"font.size": 26})
+# plt.rcParams["font.family"] = "Times New Roman"
 parser = argparse.ArgumentParser()
 
 parser.add_argument("environment")
@@ -28,26 +29,59 @@ for file in os.listdir(data_path):
     else:
         non_llm_data.append(data)
 
-llm_data = np.mean(np.vstack(llm_data), axis=0)
-non_llm_data = np.mean(np.vstack(non_llm_data), axis=0)
+llm_data = np.vstack(llm_data)
+non_llm_data = np.vstack(non_llm_data)
+
+mean_llm_data = np.mean(llm_data, axis=0)
+mean_non_llm_data = np.mean(non_llm_data, axis=0)
+
+std_llm_data = np.std(llm_data, axis=0)
+std_non_llm_data = np.std(non_llm_data, axis=0)
+
+print(np.mean(mean_llm_data) / np.mean(non_llm_data))
+print(f"{np.mean(std_llm_data)=}")
+print(f"{np.mean(std_non_llm_data)=}")
+
 
 window_size = 15
 
-
 means = list()
-for window in np.array_split(llm_data, len(llm_data) // window_size):
+stds = list()
+for window in np.array_split(mean_llm_data, len(mean_llm_data) // window_size):
     means.append(np.mean(window))
 
-plt.plot(np.arange(len(means)) * window_size, means, label="LLM reward shaping")
+for window in np.array_split(std_llm_data, len(std_llm_data) // window_size):
+    stds.append(np.mean(window))
+
+x = np.arange(len(means)) * window_size
+means = np.array(means)
+stds = np.array(stds)
+print(np.mean(stds))
+
+line_llm = plt.plot(x, means, label="LLM reward shaping", color="b", linewidth=3)
+fill_llm = plt.fill_between(
+    x, means - stds, means + stds, alpha=0.1, color="b", linewidth=4
+)
 
 means = list()
-for window in np.array_split(non_llm_data, len(non_llm_data) // window_size):
+stds = list()
+for window in np.array_split(mean_non_llm_data, len(mean_non_llm_data) // window_size):
     means.append(np.mean(window))
 
-plt.plot(np.arange(len(means)) * window_size, means, label="No LLM reward shaping")
+for window in np.array_split(std_non_llm_data, len(std_non_llm_data) // window_size):
+    stds.append(np.mean(window))
+means = np.array(means)
+stds = np.array(stds)
+print(np.mean(stds))
+
+line_non_llm = plt.plot(x, means, label="No LLM reward shaping", color="r", linewidth=3)
+fill_non_llm = plt.fill_between(
+    x, means - stds, means + stds, alpha=0.1, color="r", linewidth=4
+)
 
 plt.title("Average reward per episode on DoorKey environment")
 plt.xlabel("Episodes")
 plt.ylabel("Reward")
 plt.legend()
+
 plt.show()
--- src/test.py
+++ src/test.py
@@ -7,13 +7,14 @@ import torch
 from utils import caption_action
 
 
-env = gym.make("MiniGrid-UnlockPickup-v0", render_mode="human")
+env = gym.make("MiniGrid-KeyCorridorS3R2-v0", render_mode="human")
 
 
 obs, dog = env.reset()
 print(obs)
 obs = obs["image"]
 observation = env.step(1)[0]["image"]
+time.sleep(100)
 stringy = [[f"{cell[0]}{cell[1]}{cell[2]}" for cell in row] for row in observation]
 for row in stringy:
     print(row)
@@ -29,4 +30,3 @@ for row in stringy:
     print(row)
 
 print(caption_action(1, observation))
-env.close()
