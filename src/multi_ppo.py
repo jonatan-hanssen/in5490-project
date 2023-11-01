ec7da9d6793a2ff5c047e99f1f5826fbebaa2e81
Jonatan Hanssen <jonatan.hanssen@outlook.com>
2023-11-01 15:40:48 +0100

    Add multi_ppo
    Signed-off-by: Jonatan Hanssen <jonatan.hanssen@outlook.com>

--- src/multi_ppo.py
+++ src/multi_ppo.py
@@ -1,4 +1,78 @@
 from ppo import PPO
 from utils import llama2_policy
 
-consigliere = llama2_policy(goal, cos_sim_threshold=0, similarity_modifier=0.1)
+consigliere = llama2_policy("use the key to open the door and then get to the goal", cos_sim_threshold=0, similarity_modifier=0.1)
+
+print("Starting policy 2")
+ppo = PPO(
+        "hyperparams.json",
+        result_file="data/doorkey_big_policy2",
+        reward=False,
+        policy=True,
+        consigliere=consigliere,
+    )
+ppo.agent.consigliere.reset_cache()
+ppo.train()
+
+print("Starting policy 3")
+ppo = PPO(
+        "hyperparams.json",
+        result_file="data/doorkey_big_policy3",
+        reward=False,
+        policy=True,
+        consigliere=consigliere,
+    )
+ppo.agent.consigliere.reset_cache()
+ppo.train()
+
+print("Starting policy 4")
+ppo = PPO(
+        "hyperparams.json",
+        result_file="data/doorkey_big_policy4",
+        reward=False,
+        policy=True,
+        consigliere=consigliere,
+    )
+ppo.agent.consigliere.reset_cache()
+ppo.train()
+
+print("Starting policy 5")
+ppo = PPO(
+        "hyperparams.json",
+        result_file="data/doorkey_big_policy5",
+        reward=False,
+        policy=True,
+        consigliere=consigliere,
+    )
+ppo.agent.consigliere.reset_cache()
+ppo.train()
+
+print("Starting policy 6")
+ppo = PPO(
+        "hyperparams.json",
+        result_file="data/doorkey_big_policy6",
+        reward=False,
+        policy=True,
+        consigliere=consigliere,
+    )
+ppo.train()
+
+print("Starting policy 7")
+ppo = PPO(
+        "hyperparams.json",
+        result_file="data/doorkey_big_policy7",
+        reward=False,
+        policy=True,
+        consigliere=consigliere,
+    )
+ppo.train()
+
+print("Starting policy 8")
+ppo = PPO(
+        "hyperparams.json",
+        result_file="data/doorkey_big_policy8",
+        reward=False,
+        policy=True,
+        consigliere=consigliere,
+    )
+ppo.train()
